31072c82dc2b637721ab999f4f351183811a9b9e
Yuba <sevftgph2@gmail.com>
2021-10-29 21:07:45 +0900

    Again

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:testapp0/main.dart';
+import 'package:dietapp4/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
